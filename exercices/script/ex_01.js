9a0699a5ba0fae64fc63fdc61c16d65b893393ba
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-02 20:59:53 +0100

    sans correction

--- exercices/script/ex_01.js
+++
@@ -1,6 +0,0 @@
-var ex_01 = function(){
-let Element = document.getElementsByTagName("footer")[0];
-Element.getElementsByTagName("div")[0].innerHTML = "Hello World";
-}
-
-ex_01();
--- exercices/script/ex_02.js
+++
@@ -1,9 +0,0 @@
-var Element = document.getElementsByTagName("footer")[0];
-var div = Element.getElementsByTagName("div")[0];
-
-var count = 1;
-
-div.addEventListener("click", () => {
-    div.innerHTML = count;
-    count++;
-});
